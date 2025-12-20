8f587e71db1e16d4c09134af2f966895c490887e
=sinndnc <=sinandinc77@icloud.com>
2025-12-20 16:38:48 +0300

    Initial Commit

---
+++ Meleque/MelequeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MelequeApp.swift
+//  Meleque
+//
+//  Created by Sinan Dinç on 12/20/25.
+//
+
+import SwiftUI
+
+@main
+struct MelequeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MelequeTests/MelequeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MelequeTests.swift
+//  MelequeTests
+//
+//  Created by Sinan Dinç on 12/20/25.
+//
+
+import Testing
+@testable import Meleque
+
+struct MelequeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
